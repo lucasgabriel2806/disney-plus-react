f05d024f8c09c3589d5e9493916349c3720c0d12
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-07-07 10:54:16 -0300

    Atualizando Sidebar Responsivo #1

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 
 import GlobalApi from '../Services/GlobalApi';
 
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
+
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original';
 
 function Slider() {
@@ -28,14 +30,22 @@ function Slider() {
 
   return (
 
-    <div className='flex overflow-x-auto w-full px-16 py-4'>
-    
-      {movieList.map((item, index) => (
+    <div>
 
-        <img src={IMAGE_BASE_URL + item.backdrop_path} alt="" 
-        className='min-w-full h-[310px] object-cover object-left-top mr-5 rounded-md' />
+        <HiChevronLeft className='hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer' />
 
-      ))}
+        <HiChevronRight className='hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0' />
+
+        <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-hide' style={{overflow: 'hidden'}}>
+        
+          {movieList.map((item, index) => (
+
+            <img src={IMAGE_BASE_URL + item.backdrop_path} alt="" 
+            className='min-w-full h-[310px] object-cover object-left-top mr-5 rounded-md' />
+
+          ))}
+
+        </div>
 
     </div>
 
